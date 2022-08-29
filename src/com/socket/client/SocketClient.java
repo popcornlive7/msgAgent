88ce51f83c6ccc4933a8d3682157f4fa7c45da3a
popcornlive7 <moto0715@gmail.com>
2022-08-30 00:48:35 +0900

    CS 통신 성공

--- src/com/socket/client/SocketClient.java
+++ src/com/socket/client/SocketClient.java
@@ -8,10 +8,34 @@ import java.nio.channels.SocketChannel;
  
 public class SocketClient {
   
+
+    public static void main(String[] args){
+
+        Runnable client = new Runnable() {
+            @Override
+            public void run() {
+                 try {
+                     new SocketClient().startClient();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                 
+            }
+        };
+
+
+    new Thread(client, "client-A").start();
+    new Thread(client, "client-B").start();
+
+    }
+
     public void startClient()
             throws IOException, InterruptedException {
                 
         String sMyAddr = "192.168.1.167";
+        sMyAddr = "192.168.1.131";
         InetSocketAddress hostAddress = new InetSocketAddress(sMyAddr, 8090);
         //InetSocketAddress hostAddress = new InetSocketAddress("localhost", 8090);
         
